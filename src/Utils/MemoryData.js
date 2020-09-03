fb131646cbf6deda173880a409c9131a064cb731
1000ship <cjstjdgur1234@gmail.com>
2020-09-03 19:40:01 +0900

    fix: Use sessionStorage for memory

--- src/Utils/MemoryData.js
+++ src/Utils/MemoryData.js
@@ -3,18 +3,22 @@ var data = {};
 const MemoryData = {
   setData: (key, value) => {
     data[key] = value;
+    sessionStorage.setItem("CYFC", JSON.stringify(data))
   },
 
   getData: (key, defaultValue) => {
+    data = JSON.parse(sessionStorage.getItem("CYFC"))
     return data[key] ?? defaultValue;
   },
 
   getWholeData: () => {
+    data = JSON.parse(sessionStorage.getItem("CYFC"))
     return data;
   },
 
   clearData: () => {
     data = {};
+    sessionStorage.setItem("CYFC", JSON.stringify(data))
   },
 
 };
